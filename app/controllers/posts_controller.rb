785ead4165548ce070b492c40a5f4efbf9005ad3
qdlin <qdlin1314@gmail.com>
2016-12-29 22:45:15 +0800

    use simple_format helper to decorate

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,12 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
+
+
   def create
     @group = Group.find(params[:group_id])
     @post = Post.new(post_params)
@@ -19,10 +25,32 @@ class PostsController < ApplicationController
     end
   end
 
+  def update
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to account_posts_path(@group), notice: "Update success"
+    else
+      render :edit
+    end
+  end 
+
+  def destroy
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+
+    if @post.destroy
+      redirect_to account_posts_path(@group), alert: "文章已删除"
+    else
+      render :edit
+    end
+  end
+
   private
 
   def post_params
     params.require(:post).permit(:content)
   end
-  
+
 end
